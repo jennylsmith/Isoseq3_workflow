e5738ce9b9cd2c6476b84243ae74a83c026c94bf
Jenny Smith <jennyl.smith12@gmail.com>
2019-02-12 16:23:04 -0800

    parse the subreads.xml file for sample names

---
+++ rename_samples.sh
@@ -0,0 +1,27 @@
+#!bin/bash
+
+#Jenny Smith
+#February 11, 2019
+#purpose: rename the samples to registration number for AML samples.
+
+
+
+#mappingFile="/Users/work/Documents/GitHub/Isoseq3_workflow/samples/ibraries_submitted_to_FHCRC_genomics_core_for_PacBio.csv"
+#rawDir="r54228_20190131_194324"
+rawDir=$1
+mappingFile=$2
+
+#loop through each subdirectory 1_A01, 2_B01, 3_C01, 4_D01 under the raw data directory.
+for file in $(ls -1 $rawDir/{1..4}_*01/*.subreadset.xml)
+do
+ reg=$(cat $file |  head -1 | cut -f  4 -d " " | sed -E 's/Name=.(.+)./\1/' )
+
+ #rename each file with the patient registration number
+ #for res in $(ls -1 $(dirname $file)/*)
+ #do
+ # mv -n $res ${reg}_$res
+  #echo ${reg}_$res
+ #done
+
+ printf "$file\t$reg\n"
+done >> Sample_ID_map.txt
